b686d3afc794b4c28fcd3d32a8a1b28c84c44df9
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-09 14:26:23 -0600

    setup vehicle migration

--- new-back-end/migrations/20180709142005_vehicles.js
+++ new-back-end/migrations/20180709142005_vehicles.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('vehicles', function (table) {
+    table.increments('id');
+    table.string('make');
+    table.string('model');
+    table.integer('year');
+    table.string('note');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('vehicles');
 };
