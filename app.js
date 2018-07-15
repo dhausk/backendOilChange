a62193a18c120fff044403908065a1c65132aaaa
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-15 12:46:46 -0600

    reset cors multi connection issues resolved

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'https://oilchange.herokuapp.com/',
-  optionsSuccessStatus: 200
-}))
-
+app.use(cors({}));
 app.use('/api/vehicles', vehicles);
 app.use('/api/log', log);
 
